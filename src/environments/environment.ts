3ef86d48cbd63b9ec3d5ea5819e40b2b42686930
Sivanush <sivanushedapal@gmail.com>
2024-09-30 23:29:26 +0530

    changes in environment file

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,1 +1,6 @@
-export const environment = {};
+export const environment = {
+    production: true,
+    CLOUDINARY_CLOUD_NAME:'dgpcd5c0d',
+    CLOUDINARY_UPLOADPRESET:'CMS-CLOUDINARY',
+    apiUrl:'http://localhost:3000/api'
+};
