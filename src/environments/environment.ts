9761cb021bb5503d6d30bb0dfaed743343dc36b1
Sivanush <sivanushedapal@gmail.com>
2024-10-08 10:18:22 +0530

    server url added in prod

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,5 +2,5 @@ export const environment = {
     production: true,
     CLOUDINARY_CLOUD_NAME:'dgpcd5c0d',
     CLOUDINARY_UPLOADPRESET:'CMS-CLOUDINARY',
-    apiUrl:'http://localhost:3000/api'
+    apiUrl:'https://content-management-system-server.onrender.com/api'
 };
